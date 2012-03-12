4174acaa923434ee1b03fe8d87b2715ad047c732
Alicia Izquierdo <izquierdogarcia@gmail.com>
2012-03-12 14:24:46 +0100

    Puesto fondo claro detras de tres imagenes

--- index.php
+++ index.php
@@ -4,13 +4,14 @@ Template Name: Index Page
 */
 ?>
 <?php get_header(); ?> <!-- HEADER.PHP-->
-
+<div class="cajacolor">	
 <div id="content" class="content content-group content-index">
-	<div class="pad append-clear">		
+	<div class="pad append-clear">	
+		
 		<div class="post-group">
-							
-			<?php get_template_part('loop','index'); ?>
 			
+			<?php get_template_part('loop','index'); ?>
+			</div>
 		</div>
 	</div>
 </div>
